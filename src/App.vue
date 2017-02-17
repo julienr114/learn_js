8ffc4d8c954a44a4a0ee5b7b047c480ff1c68d78
Rabin Julien <julienr114@gmail.com>
2017-02-17 11:59:50 +0100

    Instalation vue cli et depnce

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  #app
+    nav.nav.has-shadow
+      .nav-left
+        .nav-item
+          img(src="./assets/logo_js.png", )
+          span.logo LearnJS
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+</style>
