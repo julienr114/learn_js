1dbc6935123b869f8b45bbdf1f01b10914880917
Rabin Julien <julienr114@gmail.com>
2017-02-23 13:12:25 +0100

    alert before reloading

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,17 @@ export default {
         this.progress--
       }
     }
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      window.addEventListener("beforeunload", function (e) {
+          var confirmationMessage = 'It looks like you have been editing something. '
+                                  + 'If you leave before saving, your changes will be lost.';
+
+          (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+          return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+      });
+    })
   }
 }
 </script>
