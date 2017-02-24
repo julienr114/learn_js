bb9180c55819c93cab2a34d36bdc384e8b7f28a3
Rabin Julien <julienr114@gmail.com>
2017-02-24 08:26:45 +0100

    uncomment alert on page reload

--- src/App.vue
+++ src/App.vue
@@ -80,15 +80,15 @@ export default {
     }
   },
   mounted: function () {
-    // this.$nextTick(function () {
-    //   window.addEventListener("beforeunload", function (e) {
-    //       var confirmationMessage = 'It looks like you have been editing something. '
-    //                               + 'If you leave before saving, your changes will be lost.';
-    //
-    //       (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-    //       return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
-    //   });
-    // })
+    this.$nextTick(function () {
+      window.addEventListener("beforeunload", function (e) {
+          var confirmationMessage = 'It looks like you have been editing something. '
+                                  + 'If you leave before saving, your changes will be lost.';
+
+          (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+          return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+      });
+    })
   }
 }
 </script>
